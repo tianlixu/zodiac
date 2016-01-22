4ae7ef380e2686bb5fb7c9d7a83cfdd79b6cb581
Alex Xu <tianli.xu@gmail.com>
2016-01-22 17:18:02 -0500

    added subtraction

--- goat/GoatMath.cpp
+++ goat/GoatMath.cpp
@@ -8,28 +8,69 @@ int random(int min, int max)
     return (std::rand() % (max - min + 1) + min);
 }
 
+int a, b, c;
 
-int main()
-{
+void refreshABC() {
+    a = random(0, 10);
+    b = random(0, 10);
+    c = a + b;    
+}
 
-    int a, b, c;
+void doAddition() {
+    int result;
 
-    std::srand(time(0));
-    
     while (true) {
-        a = random(0, 10);
-        b = random(0, 10);
+        refreshABC();
 
         cout <<a <<" + " <<b <<" = ";
-        cin >> c;
+        cin >> result;
 
-        while ((a+b) != c) {
+        while (result != c) {
             cout <<"Incorrect! Please try again :(" <<endl;
             cout <<a <<" + " <<b <<" = ";        
-            cin >> c;
+            cin >> result;
         }
 
         cout <<"Correct! Congratulations:)" <<endl;
+    }
+}
+
+void doSubtraction() {
+    int result;
+
+    while (true) {
+        refreshABC();
+
+        cout <<c <<" - " <<a <<" = ";
+        cin >> result;
+
+        while (result != b) {
+            cout <<"Incorrect! Please try again :(" <<endl;
+            cout <<c <<" - " <<a <<" = ";        
+            cin >> result;
+        }
+
+        cout <<"Correct! Congratulations:)" <<endl;
+    }
+}
+
+
+int main()
+{
+    std::srand(time(0));
+
+    int option;
+    cout << "Select:" <<endl;
+    cout << "1. Addition" <<endl;
+    cout << "2. subtraction" <<endl;
+    
+    cin >> option;
+    if (option == 1) {
+        doAddition();
+    } else if (option == 2) {
+        doSubtraction();
+    } else {
+
     }
 
     return 0;
